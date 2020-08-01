069aeb166d56e2eb0d1023b9092360506319e2d9
Kasia <kasia.brodzka@interia.pl>
2020-08-01 13:14:01 +0200

    translating time to industry time

--- duration.js
+++ duration.js
@@ -3,11 +3,23 @@ let Duration = (function() {
         let _ = this;
 
         _.defaults = {
-            whatever: false
+            cssClass: 'duration-initialized',
+            labels: {
+                'seconds': 'sec',
+                'minutes': 'min',
+                'hours'  : 'hrs'
+            },
+            initialValue: {
+                'seconds': '0',
+                'minutes': '0',
+                'hours':   '0'
+            },
+            leadingZeroes: false,
+            outputFormat: 'industriestunden' // or 'iso'
         }
 
         if (!settings || !settings instanceof Object) {
-            settings = {}
+            settings = {};
         }
 
         _.options = {
@@ -16,6 +28,7 @@ let Duration = (function() {
         };
 
         _.container =  undefined;
+        _.containerCssClass = 'duration-container';
         _.source    = element instanceof HTMLElement ? element : document.querySelector(element);
 
         _.init();
@@ -25,15 +38,50 @@ let Duration = (function() {
 }());
 
 Duration.prototype.buildWidget = function() {
-    let _ = this, container;
+    let _ = this, container, hours, hoursText, hoursInput, minutes, minutesText, minutesInput;
 
     container = document.createElement('div');
     container.setAttribute('id', _.generateId());
-    container.classList.add('duration-container');
+    container.classList.add(_.containerCssClass);
 
-    document.body.appendChild(container);
+    hours = document.createElement('label');
+    hours.setAttribute('data-duration','H');
+    hoursText = document.createElement('span');
+    hoursText.innerText = _.options.labels['hours'];
+    hours.appendChild(hoursText);
+    hoursInput = document.createElement('input');
+    hoursInput.setAttribute('type', 'number');
+    hoursInput.setAttribute('min', '0');
+    hoursInput.setAttribute('step', '1');
+    hoursInput.value = _.options.initialValue['hours'];
+    _.hours = hoursInput;
+    hours.appendChild(hoursInput);
+    container.appendChild(hours);
+
+    minutes = document.createElement('label');
+    minutes.setAttribute('data-duration', 'M');
+    minutesText = document.createElement('span');
+    minutesText.innerText = _.options.labels['minutes'];
+    minutes.appendChild(minutesText);
+    minutesInput = document.createElement('input');
+    minutesInput.setAttribute('type', 'number');
+    minutesInput.setAttribute('min', '0');
+    minutesInput.setAttribute('max', '59');
+    minutesInput.setAttribute('step', '1');
+    minutesInput.value = _.options.initialValue['minutes'];
+    _.minutes = minutesInput;
+    minutes.appendChild(minutesInput);
+    container.appendChild(minutes);
+    
+    _.source.insertAdjacentElement('afterend', container);
     _.container = container;
+}
 
+Duration.prototype.destroy = function() {
+    let _ = this;
+
+    _.container.parentNode.remove(_.container);
+    _.source.classList.remove(_.options.cssClass);
 }
 
 Duration.prototype.generateId = function() {
@@ -46,23 +94,53 @@ Duration.prototype.generateId = function() {
     return id;
 }
 
-Duration.prototype.handleClick = function(event) {}
-
-Duration.prototype.init = function() {
+Duration.prototype.handleInput = function(event) {
     let _ = this;
 
-    if (!_.source.classList.contains('duration-initialized')) {
-        _.buildWidget();
-        _.initializeEvents();
-
-        console.log('Duration successfully initialized!');
-
-        _.source.dispatchEvent(new Event('durationinitialized'));
+    if (event.target.closest('.' + _.containerCssClass)) {
+        _.setSourceValue();
     }
 }
 
 Duration.prototype.initializeEvents = function() {
     let _ = this;
 
-    document.addEventListener('click', _.handleClick.bind(_));
+    document.addEventListener('input', _.handleInput.bind(_));
+}
+
+Duration.prototype.setSourceValue = function() {
+    let _ = this, value;
+
+    switch(_.options.outputFormat) {
+        case 'industriestunden':
+            value = _.hours.value + ',' + Math.floor(_.minutes.value * 100 / 60);
+            break;
+        case 'iso':
+            value = 'PT';
+
+            if (_.hours.value > 0) {
+                value += _.hours.value + 'H';
+            }
+
+            if (_.minutes.value > 0) {
+                value += _.minutes.value + 'M';
+            }
+
+            value += Math.floor(_.seconds.value) + 'S';
+            break;
+    }
+
+    _.source.value = value;
+}
+
+Duration.prototype.init = function() {
+    let _ = this;
+
+    if (!_.source.classList.contains(_.options.cssClass)) {
+        _.buildWidget();
+        _.setSourceValue();
+        _.initializeEvents();
+
+        _.source.dispatchEvent(new Event('durationinitialized'));
+    }
 }
