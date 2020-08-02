f3b8f9090aed1e8cf2ad3134b2a67364753cb873
Kasia <kasia.brodzka@interia.pl>
2020-08-02 20:21:44 +0200

    switcher first draft

--- duration.js
+++ duration.js
@@ -4,18 +4,25 @@ let Duration = (function() {
 
         _.defaults = {
             cssClass: 'duration-initialized',
+            display: 'both', // or 'widget' or 'popup' or 'switcher'
             labels: {
-                'seconds': 'sec',
-                'minutes': 'min',
-                'hours'  : 'hrs'
+                seconds: 'sec',
+                minutes: 'min',
+                hours  : 'hrs'
             },
             initialValue: {
-                'seconds': '0',
-                'minutes': '0',
-                'hours':   '0'
+                seconds: '0',
+                minutes: '0',
+                hours:   '0'
             },
             leadingZeroes: false,
-            outputFormat: 'industriestunden' // or 'iso'
+            outputFormat: 'industriestunden', // or 'iso'
+            switcher: {
+                labelShows: 'alternative', //or current
+                firstLabel: 'Switch to input widget',
+                secondLabel: 'Switch to preformatted value input',
+                initialState: 'first'
+            }
         }
 
         if (!settings || !settings instanceof Object) {
@@ -26,10 +33,39 @@ let Duration = (function() {
             ..._.defaults,
             ...settings
         };
+        
+        if (settings.hasOwnProperty('initialValue')) {
+           _.options.initialValue = {
+               ..._.defaults.initialValue,
+               ...settings.initialValue
+           };
+        }
+        
+        if (settings.hasOwnProperty('switcher')) {
+            _.options.switcher = {
+                ..._.defaults.switcher,
+                ...settings.switcher
+            };
+        }
+        
+        if (settings.hasOwnProperty('labels')) {
+            _.options.labels = {
+                ..._.defaults.labels,
+                ...settings.labels
+            };
+        }
+
+        if (_.options.display === 'switcher' && _.options.switcher.labelShows === 'current' ) {
+            let tmp = _.options.switcher.firstLabel;
+            _.options.switcher.firstLabel = _.options.switcher.secondLabel;
+            _.options.switcher.secondLabel = tmp;
+        }
 
         _.container =  undefined;
         _.containerCssClass = 'duration-container';
         _.source    = element instanceof HTMLElement ? element : document.querySelector(element);
+        _.switcher = undefined;
+        _.switcherState = _.options.display === 'switcher' ? _.options.switcher.initialState === 'first' ? 'first' : 'second' : undefined;
 
         _.init();
     }
@@ -46,9 +82,11 @@ Duration.prototype.buildWidget = function() {
 
     hours = document.createElement('label');
     hours.setAttribute('data-duration','H');
+
     hoursText = document.createElement('span');
     hoursText.innerText = _.options.labels['hours'];
     hours.appendChild(hoursText);
+
     hoursInput = document.createElement('input');
     hoursInput.setAttribute('type', 'number');
     hoursInput.setAttribute('min', '0');
@@ -56,13 +94,16 @@ Duration.prototype.buildWidget = function() {
     hoursInput.value = _.options.initialValue['hours'];
     _.hours = hoursInput;
     hours.appendChild(hoursInput);
+
     container.appendChild(hours);
 
     minutes = document.createElement('label');
     minutes.setAttribute('data-duration', 'M');
+
     minutesText = document.createElement('span');
     minutesText.innerText = _.options.labels['minutes'];
     minutes.appendChild(minutesText);
+
     minutesInput = document.createElement('input');
     minutesInput.setAttribute('type', 'number');
     minutesInput.setAttribute('min', '0');
@@ -71,10 +112,30 @@ Duration.prototype.buildWidget = function() {
     minutesInput.value = _.options.initialValue['minutes'];
     _.minutes = minutesInput;
     minutes.appendChild(minutesInput);
+
     container.appendChild(minutes);
-    
-    _.source.insertAdjacentElement('afterend', container);
+
+    if (_.source.closest('label')) {
+        _.source.closest('label').insertAdjacentElement('afterend', container);
+    } else {
+        _.source.insertAdjacentElement('afterend', container);
+    }
+
     _.container = container;
+
+    switch (_.options.display) {
+        case 'widget':
+            _.source.style.setProperty('display', 'none');
+            break;
+
+        case 'popup':
+            _.setupPopup();
+            break;
+
+        case 'switcher':
+            _.setupSwitcher();
+            break;
+    }
 }
 
 Duration.prototype.destroy = function() {
@@ -100,6 +161,12 @@ Duration.prototype.handleInput = function(event) {
     if (event.target.closest('.' + _.containerCssClass)) {
         _.setSourceValue();
     }
+}
+
+Duration.prototype.hidePopup = function() {
+    let _ = this;
+
+    _.container.style.setProperty('display', 'none');
 }
 
 Duration.prototype.initializeEvents = function() {
@@ -131,6 +198,74 @@ Duration.prototype.setSourceValue = function() {
     }
 
     _.source.value = value;
+}
+
+Duration.prototype.showPopup = function() {
+    let _ = this, rect;
+
+    rect = _.source.getBoundingClientRect();
+
+    _.container.style.setProperty('position', 'fixed');
+    _.container.style.setProperty('margin', '0');
+    _.container.style.setProperty('left', rect.left + 'px');
+    _.container.style.setProperty('width', rect.width + 'px');
+    _.container.style.setProperty('top', (+rect.top + +rect.height) + 'px');
+
+    _.container.style.removeProperty('display');
+}
+
+Duration.prototype.setupPopup = function() {
+    let _ = this;
+
+    _.container.style.setProperty('display', 'none');
+
+    _.source.addEventListener('focus', _.showPopup.bind(_));
+    window.addEventListener('scroll', _.hidePopup.bind(_));
+    window.addEventListener('resize', _.hidePopup.bind(_));
+}
+
+Duration.prototype.setupSwitcher = function() {
+    let _ = this, switcher;
+
+    console.log(_.options);
+
+    switcher = document.createElement('button');
+    switcher.setAttribute('type', 'button');
+    switcher.classList.add('switcher');
+    switcher.classList.add(_.options.switcher.initialState);
+    switcher.innerHTML = '<span>' + _.options.switcher.firstLabel + '</span>' +
+                         '<span>' + _.options.switcher.secondLabel + '</span>';
+
+    _.switcher = switcher;
+    _.source.insertAdjacentElement('beforebegin', switcher);
+
+    if (_.switcherState === 'first') {
+        _.switcher.lastElementChild.setAttribute('aria-hidden', 'true');
+        _.container.style.setProperty('display', 'none');
+    } else {
+        _.switcher.firstElementChild.setAttribute('aria-hidden', 'true');
+        _.source.style.setProperty('display', 'none');
+    }
+
+    switcher.addEventListener('click', function(event) {
+       if (_.switcherState === 'first') {
+           _.source.style.setProperty('display', 'none');
+           _.container.style.removeProperty('display');
+           _.switcher.lastElementChild.removeAttribute('aria-hidden');
+           _.switcher.firstElementChild.setAttribute('aria-hidden', 'true');
+           _.switcher.classList.add('second');
+           _.switcherState = 'second';
+       } else {
+           _.source.style.removeProperty('display');
+           _.container.style.setProperty('display', 'none');
+           _.switcher.lastElementChild.setAttribute('aria-hidden', 'true');
+           _.switcher.firstElementChild.removeAttribute('aria-hidden');
+           _.switcher.classList.remove('second');
+           _.switcherState = 'first';
+       }
+
+    });
+
 }
 
 Duration.prototype.init = function() {
--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 new Duration('#demo-simple');
+
+new Duration('#demo-initial-value', {
+    initialValue: {
+        minutes: '30',
+        hours:   '1'
+    }
+});
+
+new Duration('#demo-labels', {
+    labels: {
+        minutes: 'minutes',
+        hours  : 'hours'
+    },
+});
+
+new Duration('#demo-widget', {
+    display: 'widget'
+});
+
+new Duration('#demo-popup', {
+    display: 'popup'
+});
+
+new Duration('#demo-switcher', {
+    display: 'switcher',
+    switcher: {
+        firstLabel: 'Industriezeit',
+        secondLabel: 'normale Zeit'
+    }
+});
